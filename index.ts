ac0f315d0d95809fbddcd9e31ad4b992d1f49ca5
Mo Sattler <hi@mosattler.com>
2022-10-31 17:29:32 +0100

    init

---
+++ index.ts
@@ -0,0 +1,21 @@
+import AdminJS from "adminjs";
+import AdminJSExpress from "@adminjs/express";
+import { Database, Resource } from "@adminjs/prisma";
+import { PrismaClient } from "@prisma/client";
+import { DMMFClass } from "@prisma/client/runtime";
+
+AdminJS.registerAdapter({ Database, Resource });
+
+export const getAdminRouter = (
+  prisma: PrismaClient,
+  resources: Array<any>
+) => {
+  // `_baseDmmf` contains necessary Model metadata. `PrismaClient` type doesn't have it included
+  const dmmf = (prisma as any)._baseDmmf as DMMFClass;
+
+  const admin = new AdminJS({ resources });
+
+  const router = AdminJSExpress.buildRouter(admin);
+
+  return router;
+};
